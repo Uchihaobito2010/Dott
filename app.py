b78f821942fce555ef940a045e10e6da20fad41d
Uchihaobito2010 <paraschourey08@gmail.com>
2025-12-14 17:37:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ HTML_TEMPLATE = '''
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mobile Tracker Pro - ABBAS</title>
+    <title>Number to info</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
         :root {
