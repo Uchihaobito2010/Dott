528a5292f684e0055bd0103ceb2a0bb3fe8edd52
Uchihaobito2010 <paraschourey08@gmail.com>
2025-12-14 23:22:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -777,7 +777,7 @@ def track():
         return jsonify({"success": False, "message": "Invalid mobile number"})
 
     try:
-        api_url = f"https://numapi.anshapi.workers.dev/?num={num}"
+        api_url = f"https://neonosint.onrender.com/number-to-info?apikey=cSSohzIQeSPMmSogyHM9ENMpHifNhOg5&number={n}"
         response = requests.get(api_url, timeout=15)
         api = response.json()
 
